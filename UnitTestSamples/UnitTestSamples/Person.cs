c6a056c509d227b90797c09ed6e49b503401b478
Sergey Kichuk <skichuk@luxoft.com>
2018-08-14 10:04:17 +0300

    Added person

---
+++ UnitTestSamples/UnitTestSamples/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UnitTestSamples
+{
+    public class Person
+    {
+        public int Age { get; set; }
+        
+        public DateTime DateOfBirth { get; set; }
+
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+    }
+}
